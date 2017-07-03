5806bd45f1c1161706d08219626ff696ee5e1a3f
Artur Augustyniak <artur@aaugustyniak.pl>
2017-07-03 20:03:09 +0200

    init

---
+++ src/FileLister.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Artur Augustyniak
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace Aaugustyniak\FileListerFilter;
+
+use \Iterator as Iterator;
+
+/**
+ * @author Artur Augustyniak <artur@aaugustyniak.pl>
+ */
+class FileLister implements Iterator
+{
+    private $position = 0;
+    private $array = array(
+        "firstelement",
+        "secondelement",
+        "lastelement",
+    );
+
+    public function __construct()
+    {
+        $this->position = 0;
+    }
+
+    function rewind()
+    {
+        $this->position = 0;
+    }
+
+    function current()
+    {
+        return $this->array[$this->position];
+    }
+
+    function key()
+    {
+        return $this->position;
+    }
+
+    function next()
+    {
+        ++$this->position;
+    }
+
+    function valid()
+    {
+        return isset($this->array[$this->position]);
+    }
+}
